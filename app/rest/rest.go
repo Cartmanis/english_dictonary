9e06d1f48dc06842f91671dcf8f0f5e8a3a78955
cartmani <VShmelcer@gmail.com>
2019-05-17 22:21:01 +0700

    очистка проекта

--- app/rest/rest.go
+++ app/rest/rest.go
@@ -3,15 +3,11 @@ package rest
 import (
 	"english_dictonary/app/cmd/lg"
 	"english_dictonary/app/provider_db"
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
 	R "github.com/go-pkgz/rest"
 	"net/http"
-	"os"
 	"strconv"
-	"time"
 )
 
 type Rest struct {
@@ -35,29 +31,8 @@ func (s *Rest) Run() error {
 	r.Post("/login", s.login)
 	r.Post("/logout", s.logout)
 
-	//авторизация
-	//r.Get("/get-token", GetTokenHandler)
-
-	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static",
-	//	http.FileServer(http.Dir("./static"))))
-
 	return http.ListenAndServe(":"+strconv.Itoa(s.port), r)
 }
-
-var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	token := jwt.New(jwt.SigningMethodHS256)
-	token.Header["admin"] = true
-	token.Header["name"] = "Logiiin"
-	token.Header["exp"] = time.Now().Add(time.Hour * 24).Unix()
-	tokenString, err := token.SignedString(os.Getenv("SECRET_KEY"))
-
-	if err != nil {
-		fmt.Println("[ERROR]: ", err)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	w.Write([]byte(tokenString))
-})
 
 func SendJSON(w http.ResponseWriter, r *http.Request, status int, i interface{}) {
 	render.Status(r, status)
