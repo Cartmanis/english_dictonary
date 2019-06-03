12b9485fcd7aadd49321f984f361aa067941281e
carmanis <vshmelcer@gmail.com>
2019-06-03 17:37:46 +0700

    компонент авторизации

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,43 +1,41 @@
 <template>
-  <v-dialog v-model="auth"  max-width="500px">
-    <v-card>
-      <v-toolbar height="60px">
-        <v-toolbar-title>Авторизация</v-toolbar-title>
-      </v-toolbar>
-      <v-card-text>
-        <v-layout wrap>
-          <v-flex xs12>
-            <v-text-field  prepend-icon="person" label="Имя пользователя"
-                           :rules="[rules.required]"
-            ></v-text-field>
-            <v-text-field
-                v-model='password.text' prepend-icon="lock" label="Пароль"
-                 :type= "password.show? 'text' :'password'"
-                :append-icon="password.show ? 'visibility' : 'visibility_off'"
-                @click:append="password.show=!password.show"
-                :rules="[rules.required]"></v-text-field>
-          </v-flex>
-          <v-flex xs8>
-            <v-btn @click="onLogin" color = "primary">Вход</v-btn>
-          </v-flex>
-          <v-flex xs4>
-            <v-btn @click="onRegistration" flat color = "primary"
-
-             >Регистрация
-            </v-btn>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm4 md3>
+        <v-card class="elevation-12">
+          <v-toolbar>
+            <v-toolbar-title>Авторизация</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field  prepend-icon="person" label="Имя пользователя"></v-text-field>
+              <v-text-field
+                      v-model='password.text' prepend-icon="lock" label="Пароль"
+                      :type= "password.show? 'text' :'password'"
+                      :append-icon="password.show ? 'visibility' : 'visibility_off'"
+                      @click:append="password.show=!password.show">
+              </v-text-field>
+              <h4 v-if="error" align="center" style="color: red;">имя пользователя или пароль введены не верно</h4>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary">Вход</v-btn>
+            <v-btn color="primary">Закрыть</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn flat color="info">Регистрация</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
-
 <script>
     export default {
         name: "Login",
       data () {
         return {
-          auth: true,
+          error: false,
           login: "",
           password: {
             text: "",
@@ -48,12 +46,24 @@
           }
         }
       },
+      props: {
+          hidePassword: {
+            type: Boolean
+          }
+      },
       methods: {
-        onLogin() {
-          console.log('попали')
+        onChange () {
+          this.$emit('change-data', this.settings)
         },
-        onRegistration() {
-          console.log('регистрация')
+        async logIn () {
+          const res = await controller.login(this.auth.login, this.auth.password)
+          console.log(res.data.error, ' - ', res.data.is_login)
+          if (!res.data.error && res.data.is_login === 'true') {
+            window.localStorage.setItem('login', true)
+            this.$router.push('/admin')
+          } else {
+            this.error = true
+          }
         }
       }
     }
